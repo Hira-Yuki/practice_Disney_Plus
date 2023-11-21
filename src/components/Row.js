8fb94537ec064c800fc6a96a45439192487ed076
Hira-Yuki <cjsdlf44@gmail.com>
2023-11-21 17:59:31 +0900

    [feat] swiper 이용해서 네비게이션 기능 및 스크롤 기능 추가 - 강의에서 제공된 코드 이용시 모듈을 찾을 수 없는 문제 발견함 - swiper 문서 확인시 사용 방법 및 파일 경로가 일부 변경된 것을 확인하여 문서 기반으로 수정하여 해결함

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,15 +1,22 @@
-import React, { useEffect, useState, useCallback, useRef } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import axios from "../api/axios";
 import "./Row.css";
 import MovieModal from "./MovieModal";
+import styled from "styled-components";
+
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import swiper style
+import 'swiper/css';
+import "swiper/css/scrollbar";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const Row = ({ title, id, fetchUrl }) => {
-
   const [movies, setMovies] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [movieSelected, setMovieSelected] = useState({});
-  // 스크롤 제어를 위한 useRef 사용
-  const scrollContainerRef = useRef(null);
 
   const fetchMovieData = useCallback(async () => {
     try {
@@ -18,67 +25,113 @@ const Row = ({ title, id, fetchUrl }) => {
       return request;
     } catch (error) {
       // 에러 처리를 위한 분기
-      console.error("영화 데이터를 가져오는 중 오류 발생, themoviedb 관리자에게 문의하세요. :", error);
+      console.error(
+        "영화 데이터를 가져오는 중 오류 발생, themoviedb 관리자에게 문의하세요. :",
+        error
+      );
     }
   }, [fetchUrl]);
-
-  const handleScroll = (scrollOffset) => {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollLeft += scrollOffset;
-    }
-  };
 
   useEffect(() => {
     fetchMovieData();
   }, [fetchMovieData]);
 
   const handleClick = (movie) => {
-    setModalOpen(true)
-    setMovieSelected(movie)
-  }
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
 
   return (
-    <div>
+    <Container>
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span
-            className="arrow"
-            onClick={() => {
-              handleScroll(-(window.innerWidth - 80));
-            }}
-          >
-            {"<"}
-          </span>
-        </div>
-        <div ref={scrollContainerRef} id={id} className="row__posters">
-          {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className="row__poster"
-              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-              alt={movie.name}
-              onClick={ () => handleClick(movie)}
-            />
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true} //loop 기능을 사용할 것인지
+        navigation // arrow 버튼 사용 유무
+        pagination={{ clickable: true }} //페이지 버튼 보이게 할지
+        breakpoints={{
+          1378: {
+            slidesPerView: 6, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 3,
+          },
+        }}
+      >
+        <Content id={id}>
+          {movies.map(movie => (
+            <SwiperSlide key={movie.id}>
+              <Wrap>
+                <img
+                  key={movie.id}
+                  src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                  alt={movie.name}
+                  onClick={() => handleClick(movie)}
+                />
+              </Wrap>
+            </SwiperSlide>
           ))}
-        </div>
-        <div className="slider__arrow-right">
-          <span
-            className="arrow"
-            onClick={() => {
-              handleScroll(window.innerWidth - 80);
-            }}
-          >
-            {">"}
-          </span>
-        </div>
-      </div>
-      {modalOpen && 
-      
-      <MovieModal {...movieSelected} setModalOpen={setModalOpen}/>
+        </Content>
+      </Swiper>
+
+
+      {modalOpen &&
+        <MovieModal
+          {...movieSelected}
+          setModalOpen={setModalOpen}
+        />
       }
-    </div>
+    </Container>
   );
 };
 
 export default Row;
+
+const Container = styled.div`
+  padding: 0 0 26px;
+`;
+
+const Content = styled.div``;
+
+const Wrap = styled.div`
+  width: 95%;
+  height: 95%;
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0/69%) 0px 26px 30px -10px,
+    rgb(0 0 0/73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    width: 100%;
+    transition: opacity 500ms ease-in-out;
+    z-index: 1;
+  }
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(0.98);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
